c104775d0da32f8a10fe89da81bf3c9098630706
Asir Hameem <asirhameem@gmail.com>
2025-06-11 19:43:43 +0600

    feat: added navigation to product add page

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@heroui/react";
 import { useQuery } from "@tanstack/react-query";
 import { PlusIcon } from "lucide-react";
+import { useNavigate } from "react-router";
 import { ProductTable } from "../../components/product/ProductTable";
 import { getAllProducts } from "../../service/product.service";
 
 export default function ProductPage() {
+  const navigate = useNavigate();
   const { data, isFetching } = useQuery({
     queryKey: ["products"],
     queryFn: getAllProducts,
@@ -25,7 +27,7 @@ export default function ProductPage() {
       <h1 className="mb-3 text-center text-xl font-bold">All Products</h1>
       <div className="mb-3 flex items-center justify-between">
         <p>Total Products: {data.total_products}</p>
-        <Button>
+        <Button onClick={() => navigate("/product/add")}>
           <PlusIcon className="size-4" />
           Add Product
         </Button>
