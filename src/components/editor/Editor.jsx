9f769c67100c1d026591e8cc213851e8025ae392
Asir Hameem <asirhameem@gmail.com>
2025-06-11 23:30:12 +0600

    feat: added quill editor

---
+++ src/components/editor/Editor.jsx
@@ -0,0 +1,59 @@
+import Quill from "quill";
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "./Editor.css";
+
+import { forwardRef, useEffect, useLayoutEffect, useRef } from "react";
+
+const Editor = forwardRef(({ readOnly, defaultValue, onTextChange, onSelectionChange }, ref) => {
+  const containerRef = useRef(null);
+  const defaultValueRef = useRef(defaultValue);
+  const onTextChangeRef = useRef(onTextChange);
+  const onSelectionChangeRef = useRef(onSelectionChange);
+
+  useLayoutEffect(() => {
+    onTextChangeRef.current = onTextChange;
+    onSelectionChangeRef.current = onSelectionChange;
+  });
+
+  useEffect(() => {
+    ref.current?.enable(!readOnly);
+  }, [ref, readOnly]);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const editorContainer = container.appendChild(container.ownerDocument.createElement("div"));
+    const quill = new Quill(editorContainer, {
+      theme: "snow",
+    });
+
+    ref.current = quill;
+
+    if (defaultValueRef.current) {
+      quill.setContents(defaultValueRef.current);
+    }
+
+    quill.on(Quill.events.TEXT_CHANGE, (...args) => {
+      onTextChangeRef.current?.(...args);
+    });
+
+    quill.on(Quill.events.SELECTION_CHANGE, (...args) => {
+      onSelectionChangeRef.current?.(...args);
+    });
+
+    return () => {
+      ref.current = null;
+      container.innerHTML = "";
+    };
+  }, [ref]);
+
+  return (
+    <div ref={containerRef}>
+      <div className="ql-editor" dangerouslySetInnerHTML={{ __html: defaultValue }} />
+    </div>
+  );
+});
+
+Editor.displayName = "Editor";
+
+export default Editor;
