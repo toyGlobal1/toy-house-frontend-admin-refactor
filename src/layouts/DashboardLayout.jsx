f345d7cd013d3c5f917f502c40bc5048a4083f97
ZBM Zubayer <zbmzubayer@gmail.com>
2025-06-28 00:29:21 +0600

    fix: remove redundant toast provider

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,4 +1,3 @@
-import { ToastProvider } from "@heroui/react";
 import { Suspense } from "react";
 import { Outlet } from "react-router";
 import { LoadingScreen } from "../components/ui/LoadingScreen";
@@ -7,7 +6,6 @@ import DashboardSidebar from "./DashboardSidebar";
 export default function DashboardLayout() {
   return (
     <div className="flex max-h-screen">
-      <ToastProvider />
       <DashboardSidebar />
       <main className="flex-1 overflow-y-auto p-4">
         <Suspense fallback={<LoadingScreen />}>
